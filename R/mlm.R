b500c248466b71bc31fb72ae0be5ddc407fe4a22
Carson Zhang <carsonzhang4@gmail.com>
2024-01-12 00:34:54 +0100

    Skeleton documentation. mlm and method implemenentations can be loaded now. Some tests written.

--- R/mlm.R
+++ R/mlm.R
@@ -1,22 +1,33 @@
 library(checkmate)
 library(testthat)
-library(data.table)
 
+#' Fit an OLS multiple regression model
+#'
+#' @param data A data frame with only numeric variables and no missing data.
+#' @param response_col_name The name of the response variable.
+#'
+#' @return An object of class "mlm".
+#' @export
+#'
+#' @examples
 mlm <- function(data, response_col_name) {
   # type checks
-  assert_data_frame(data)
+  assert_data_frame(data, types = "numeric")
   assert_string(response_col_name)
+
+  # missingness check
+  assert(!anyMissing(data))
 
   # check that response variable is valid
   all_var_names <- names(data)
   assert_true(response_col_name %in% all_var_names)
 
   Y <- data[, response_col_name]
-  X_no_intercept <- data[, -response_col_name]
+  X_no_intercept <- data[, -which(names(data) == response_col_name)]
 
   # for simplicity, we only allow numeric variables
-  assert_numeric(X_no_intercept, any.missing = FALSE)
-  assert_numeric(Y, any.missing = FALSE)
+  # lapply(X_no_intercept, assert_numeric(., any.missing = FALSE))
+  # assert_numeric(Y, any.missing = FALSE)
 
   n <- nrow(X_no_intercept)
   p <- ncol(X_no_intercept)
@@ -25,13 +36,14 @@ mlm <- function(data, response_col_name) {
   # when the number of coefficients is not greater than number of observations
   assert_false((p + 1) > n)
 
-  X <- cbind(intercept = rep(1, times = n), X_no_intercept)
+  X <- cbind(intercept = rep(1, times = n), X_no_intercept) |>
+    as.matrix()
   Y <- data[, response_col_name]
 
   # solve the normal equations to compute the OLS estimate of the coefficients
-  I_p <- diag(p)
+  I_p <- diag(p + 1)
   XT_X_inv <- solve(a = t(X) %*% X, b = I_p)
-  Beta_hat <- XT_X_inv %*% t(X) %*% Y
+  Beta_hat <- as.vector(XT_X_inv %*% t(X) %*% Y)
 
   # add names to the coefficient estimates
   pred_var_names <- names(X_no_intercept)
@@ -41,17 +53,37 @@ mlm <- function(data, response_col_name) {
   structure(list(
     predictors = X_no_intercept,
     response = Y,
-    coefficients = Beta_hat,
+    response_col_name = response_col_name,
+    coefficients = Beta_hat
   ), class = "mlm")
 }
 
+#' Print basic information about an mlm object
+#'
+#' @param mlm_mod An object of class "mlm"
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
 print.mlm <- function(mlm_mod, ...) {
-  cat("Multiple Linear Regression Model")
+  cat("Multiple Linear Regression Model\n\n")
   print(mlm_mod$coefficients)
   invisible(mlm_mod)
 }
 
-predict.mlm(mlm_mod, newdata, ...) {
+#' Generate predictions from an mlm model
+#'
+#' @param mlm_mod An object of class "mlm"
+#' @param newdata
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+predict.mlm <- function(mlm_mod, newdata, ...) {
   # type checks
   assert_equal(ncol(newdata), ncol(mlm_mod$predictors))
   assert_numeric(X_no_intercept, any.missing = FALSE)
--- tests/testthat/test_mlm.R
+++ tests/testthat/test_mlm.R
@@ -1,9 +1,30 @@
-# instantiate models for the following cases
-# TODO: combinations of these cases
+library(palmerpenguins)
 
-# p < n
-mlm_mod <- mlm(data = mtcars, response_col_name = "mpg")
-lm_mod <- lm(mpg ~ ., data = mtcars)
+# instantiate models for the following cases
+# TODO: consider testing combinations of these cases
+
+# working models: p > n
+assert(ncol(mtcars) < nrow(mtcars))
+mlm_mod_mtcars <- mlm(data = mtcars, response_col_name = "mpg")
+lm_mod_mtcars <- lm(mpg ~ ., data = mtcars)
+
+# non-numeric predictor
+assert(ncol(penguins) < nrow(penguins))
+mlm_mod_penguins <- mlm(data = penguins, response_col_name = "species")
+
+# non-numeric response
+assert(ncol(penguins) < nrow(penguins))
+mlm_mod_penguins <- mlm(data = penguins, response_col_name = "species")
+
+# tests for coefficients
+testthat::expect_equal(mlm_mod_mtcars$coefficients, lm_mod_mtcars$coefficients)
+
+# tests for printing
+testthat::expect_output(print(mlm(data = mtcars, response = "")))
+
+testthat::expect_equal(print, base::print)
+
+# tests for predictions
 
 # p == n
 mtcars_square_idx <- sample(1:nrow(mtcars), ncol(mtcars), replace = FALSE)
@@ -11,26 +32,4 @@ mtcars_square <- mtcars[mtcars_square_idx, ]
 
 # p > n
 mtcars_highdim_idx <- sample(1:nrow(mtcars), ncol(mtcars) - 1, replace = FALSE)
-mtcars_highdim <- mtcars[mtcars_himdim_idx, ]
-
-# non-numeric predictor
-mtcars_non_num <- mtcars
-
-# non-numeric response
-
-# tests for printing
-testthat::expect_output(print(mlm(data = mtcars, response = "")))
-
-testthat::expect_equal(print, base::print)
-
-# tests for coefficients
-
-
-
-
-
-# tests for predictions
-
-
-
-
+mtcars_highdim <- mtcars[mtcars_highdim_idx, ]
